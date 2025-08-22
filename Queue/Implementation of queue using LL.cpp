a8183f532a4d280be8ca6b586dcc1ee3122afd15
SubhanAlom009 <subhanalombasic123@gmail.com>
2025-08-23 00:45:06 +0530

    binary search in array

--- Queue/Implementation of queue using LL.cpp
+++ Queue/Implementation of queue using LL.cpp
@@ -1,34 +1,92 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <string>
+#include <set>
 
 using namespace std;
 
-int binarySearch(vector<int> arr, int val)
+class Node
 {
-    int low = 0;
-    int high = arr.size() - 1;
+public:
+    int data;
+    Node *next;
 
-    while (low <= high)
+public:
+    Node(int data1)
     {
-        int mid = (low + high) / 2;
-
-        if (arr[mid] == val)
-            return mid;
-        else if (arr[mid] > val)
-        {
-            high = mid - 1;
-        }
-        else
-        {
-            low = mid + 1;
-        }
+        data = data1;
+        next = nullptr;
     }
-    return -1;
-}
+};
+
+class queue
+{
+public:
+    Node *front;
+    Node *rear;
+    int queueSize;
+
+    queue()
+    {
+        front = nullptr;
+        rear = nullptr;
+        queueSize = 0;
+    }
+
+    void enqueue(int x)
+    {
+        Node *temp = new Node(x);
+        if (rear == NULL && front == NULL)
+        {
+            front = rear = temp;
+            queueSize++;
+            return;
+        }
+        rear->next = temp;
+        rear = temp;
+        queueSize++;
+    }
+
+    void dequeue()
+    {
+        if (front == NULL)
+        {
+            cout << "Queue is empty" << endl;
+            return;
+        }
+        Node *temp = front;
+        front = front->next;
+        delete temp;
+        queueSize--;
+    }
+
+    int top()
+    {
+        if (front == nullptr)
+        {
+            cout << "Queue is empty" << endl;
+            return -1;
+        }
+        return front->data;
+    }
+
+    int size()
+    {
+        return queueSize;
+    }
+};
 
 int main()
 {
-    vector<int> arr = {1, 2, 3, 4, 5, 6};
+    queue q;
 
-    cout << binarySearch(arr, 5);
+    q.enqueue(10);
+    q.enqueue(20);
+    q.enqueue(30);
+
+    cout << "Top of the Queue: " << q.top() << endl;
+    q.dequeue();
+    cout << "Top after dequeue: " << q.top() << endl;
+    cout << "Size of Queue: " << q.size() << endl;
 }
